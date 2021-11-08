ba0533ba263e5b3bc041b008c0822a78c7b08300
Ana Beatriz Guassu <79941051+annaguassu@users.noreply.github.com>
2021-11-08 10:30:37 -0300

    Aula 5

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,11 @@
+let qualquer = "legal"
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 4.6892
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//evitar nomes genéricos
+let valor = ""
+let numero = 1
