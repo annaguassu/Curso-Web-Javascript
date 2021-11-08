5606531a33f0bd3e99f86c9adb531e0e7d9a9dcd
Ana Beatriz Guassu <79941051+annaguassu@users.noreply.github.com>
2021-11-08 10:59:14 -0300

    Aula 13

---
+++ Fundamentos/atribuicao.js
@@ -0,0 +1,13 @@
+const a = 7
+let b = 3
+
+b += a //acrescentando o valor de a (b = b + a)
+console.log(b)
+b -= 4 //(b = b - 4)
+console.log(b)
+b *= 2 //(b = b * 2
+console.log(b)
+b /= 2 //(b = b / 2)
+console.log(b)
+b %= 2 //(b = b % 2)
+console.log(b)
