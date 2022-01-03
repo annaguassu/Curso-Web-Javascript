88d9b148fd816d57ba39fa1798574d8012f58b18
Ana Beatriz Guassu <79941051+annaguassu@users.noreply.github.com>
2022-01-03 08:28:10 -0300

    Função base e expoente

---
+++ Exercicios Javascript/exec3.js
@@ -0,0 +1,7 @@
+function Potencia(b, e){
+    return console.log(Math.pow(b, e))
+}
+
+Potencia(9,2)
+
+/* Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
