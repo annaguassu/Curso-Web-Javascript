3a29047c40315e5d89fc23f4b46a888907a6e6de
Ana Beatriz Guassu <79941051+annaguassu@users.noreply.github.com>
2022-01-03 08:13:50 -0300

    exec 1

---
+++ Exercicios Javascript/exec1.js
@@ -0,0 +1,12 @@
+function Calc(a, b, operacao){
+   if (operacao === "+") return a + b
+   if (operacao === "-") return a - b
+   if (operacao === "/") return a / b
+   if (operacao === "*") return a * b
+
+}
+
+console.log(Calc(3,6, "*"))
+
+/*Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.
