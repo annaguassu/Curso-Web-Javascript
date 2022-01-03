21ed722eb99d631dd91f1bb739cf4df204c6e1af
Ana Beatriz Guassu <79941051+annaguassu@users.noreply.github.com>
2022-01-03 08:36:58 -0300

    Divisão e resto

---
+++ Exercicios Javascript/exec4.js
@@ -0,0 +1,7 @@
+function Divisao(dividendo, divisor){
+    return console.log(dividendo / divisor, dividendo % divisor)
+}
+
+Divisao(47, 3)
+
+/*Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão destes dois valores.*/
