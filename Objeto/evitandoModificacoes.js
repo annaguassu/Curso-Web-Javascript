16ad58ae7eb5ba3014cb3cd555451015c2b8c523
Ana Beatriz Guassu <79941051+annaguassu@users.noreply.github.com>
2022-01-21 08:58:51 -0300

    Aula 24

---
+++ Objeto/evitandoModificacoes.js
@@ -0,0 +1,22 @@
+//Object.preventExtensions --> pode excluir, mas n add
+const produto = Object.preventExtensions({
+  nome: 'Qualquer', preco: 1.99, tag: 'promoção'
+})
+
+console.log('Extensivel:', Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.descricao = 'Borracha escolar branca'
+delete produto.tag
+console.log(produto)
+
+//Object.seal --> alterar os valores existentes, mas não pode add nem excluir
+
+const pessoa = {nome: 'Ana', idade: 35}
+Object.seal(pessoa)
+console.log('Selado:', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+console.log(pessoa)
